6c69ed3849046acb90be20a881d79fa8233989a4
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-02-05 13:45:18 +0700

    feat: experiment build 1.0.1

--- Package.swift
+++ Package.swift
@@ -21,13 +21,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.0/SequreSDK.xcframework.zip",
-            checksum: "d785469483ed9acefb06a5610bc875985295e27ac0d7577ae606f4493e8ad684"
+            // path: "binary/SequreSDK.xcframework",
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.1/SequreSDK.xcframework.zip",
+            checksum: "03375ecbe22948fa683b829b278b53a02ec3412fdde84c00f90c3209dd9f48ee"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.0/SequreSDKLite.xcframework.zip",
-            checksum: "7f8de843c1d3e78d148943b9df11ec96badaa7d3720d6e8ec791e4dec689a888"
+            // path: "binary/SequreSDK.xcframework",
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.1/SequreSDKLite.xcframework.zip",
+            checksum: "e543dc9755cc84a767ce7cdbe02340a9bfdbee6c07e6391bdc717bec7f63530c"
         ),
     ],
 )
