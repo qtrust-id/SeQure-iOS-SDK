1b174bfce73c9a28d0767a896779d70426d56de8
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-02-05 12:59:26 +0700

    feat: change configuration sdk

--- Package.swift
+++ Package.swift
@@ -21,11 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            path: "binary/SequreSDK.xcframework"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.0/SequreSDK.xcframework.zip",
+            checksum: "d785469483ed9acefb06a5610bc875985295e27ac0d7577ae606f4493e8ad684"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            path: "binary/SequreSDKLite.xcframework"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.0/SequreSDKLite.xcframework.zip",
+            checksum: "7f8de843c1d3e78d148943b9df11ec96badaa7d3720d6e8ec791e4dec689a888"
         ),
     ],
 )
