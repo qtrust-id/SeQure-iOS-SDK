d2a2875fd5fe80d641f12f5ed3c73277c7a45dde
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-02-18 16:13:11 +0700

    fix: try handle prod issue SSL error (-1200)

--- Package.swift
+++ Package.swift
@@ -21,13 +21,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDK.xcframework.zip",
-            checksum: "dc8eb274961e774de35b3eb232f573dc33958efbfdb56478fdbd340f0c866e31"
+            path: "binary/SequreSDK.xcframework",
+            // url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDK.xcframework.zip",
+            // checksum: "dc8eb274961e774de35b3eb232f573dc33958efbfdb56478fdbd340f0c866e31"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDKLite.xcframework.zip",
-            checksum: "834a2ed5c967ed10a80eb26df58cf63e440939fcffd779087de430843eb0c8a9"
+            path: "binary/SequreSDKLite.xcframework",
+            // url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDKLite.xcframework.zip",
+            // checksum: "834a2ed5c967ed10a80eb26df58cf63e440939fcffd779087de430843eb0c8a9"
         ),
     ],
     swiftLanguageVersions: [
---
+++ binary/SequreSDK.xcframework/ios-arm64/SequreSDK.framework/Headers/SequreSDK.h
@@ -0,0 +1,19 @@
+//
+//  SequreSDK.h
+//  SequreSDK
+//
+//  Created by IceQwen on 27/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SequreSDK.
+FOUNDATION_EXPORT double SequreSDKVersionNumber;
+
+//! Project version string for SequreSDK.
+FOUNDATION_EXPORT const unsigned char SequreSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SequreSDK/PublicHeader.h>
+#import <SequreSDK/OpenCVWrapper.h>
+
+
