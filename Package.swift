3f45d068c62e929998000e4151bff345dacdfec4
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-02-18 18:09:51 +0700

    feat: bump version to 1.0.9

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDK.xcframework.zip",
-            checksum: "dc8eb274961e774de35b3eb232f573dc33958efbfdb56478fdbd340f0c866e31"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.9/SequreSDK.xcframework.zip",
+            checksum: "547e5db91db1feeb044559479836da1dba176f17e6bc017914b8ac0b2c4dd250"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.8/SequreSDKLite.xcframework.zip",
-            checksum: "834a2ed5c967ed10a80eb26df58cf63e440939fcffd779087de430843eb0c8a9"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.0.9/SequreSDKLite.xcframework.zip",
+            checksum: "1272bc2591ce013d4a7532b9f057b494c80e962cd3bf0dbd21d89330f45ec41a"
         ),
     ],
     swiftLanguageVersions: [
