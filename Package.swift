6ff3f09ca89a707e1677d78fcf8af7bf7c13f01e
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-03-25 09:57:00 +0700

    build: Update marketing version to 1.1.2 and enhance network services with connectivity waiting and an increased request timeout.

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.1.1/SequreSDK.xcframework.zip",
-            checksum: "988c9a532889c112e7f4a3df9479a5b7e46da005cafbf5d928c0fdf9e48e8e97"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.1.2/SequreSDK.xcframework.zip",
+            checksum: "e093b18399cd56e0b026693253df66b184e318afa7ec506dd76a2fe48c90e075"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.1.1/SequreSDKLite.xcframework.zip",
-            checksum: "3a16cc24cd1bb52a2effb968d85f0986805d4d664ec2be1faf29e57bb63eff9f"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/1.1.2/SequreSDKLite.xcframework.zip",
+            checksum: "799cbded6010931be0782c4209d61aa80f6ecc06422610e12e10d34c5b0a62a6"
         ),
     ],
     swiftLanguageVersions: [
