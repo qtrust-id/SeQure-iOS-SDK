049df8369b129a9bdeb94af066591290077603be
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-07-03 16:04:27 +0700

    chore: update SequreSDK and SequreSDKLite to version 2.1.11

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.10/SequreSDK.xcframework.zip",
-            checksum: "267234295000a7d423da121e8ac4c91bb3e1f575aae31ccc519f7fafb19a5067"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.11/SequreSDK.xcframework.zip",
+            checksum: "7a93ba3b1ab5904e8b04b17d9ce6d625106f750cff1c6ac7123c66eb6df54985"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.10/SequreSDKLite.xcframework.zip",
-            checksum: "0ba7b5d1d90edb38c734ed41059deadbcf6f8491a494aee534a76c8582e8c274"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.11/SequreSDKLite.xcframework.zip",
+            checksum: "9d59339bd137ab57259f728680b455b04fd3e31fc52d568fd77c81fb473d26d6"
         ),
     ],
     swiftLanguageVersions: [
