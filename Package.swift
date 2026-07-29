52267a2b427e4b2155ebc572e48fcd79d9201cdc
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-07-29 18:20:54 +0700

    chore: bump SeQure SDK version to 2.1.14 and update binary checksums

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.13/SequreSDK.xcframework.zip",
-            checksum: "aef5a8a8006d7a28d743aa472e0c835bd2ed8aea67e2be6e1af117aff2a79a47"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.14/SequreSDK.xcframework.zip",
+            checksum: "08fa4f30c19cce4a856cb8a180781112ed5281e28e7b06e9e7f98ba3aee65b32"
         ),
         .binaryTarget(
             name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.13/SequreSDKLite.xcframework.zip",
-            checksum: "5990ca805a9ff901ddf0349299d582cfb182fff1e87cafba68e069ea18212c29"
+            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.1.14/SequreSDKLite.xcframework.zip",
+            checksum: "1cf37eeafd0083bbbe264c4bb0f0e854e964f4ef72c84617b1a1c7f203f9ded7"
         ),
     ],
     swiftLanguageVersions: [
