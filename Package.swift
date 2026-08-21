b7a4b4e07decabce94efb0548286402debf7b496
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-08-21 17:29:12 +0700

    chore: update SDK version to 2.3.0 and migrate to local xcframework path

--- Package.swift
+++ Package.swift
@@ -12,24 +12,15 @@ let package = Package(
         .library(
             name: "SequreSDK",
             targets: ["SequreSDK"]
-        ),
-        .library(
-            name: "SequreSDKLite",
-            targets: ["SequreSDKLite"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "SequreSDK",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.2.1/SequreSDK.xcframework.zip",
-            checksum: "39223f7ecb5bcadae39f4db532fd38782052422a7cfb1b6133a7a032a441a29e"
-        ),
-        .binaryTarget(
-            name: "SequreSDKLite",
-            url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.2.1/SequreSDKLite.xcframework.zip",
-            checksum: "1036dae82bd190e204c637a6771814ffc956fc6a2ae5647f7e40c2383c2b3cf9"
-        ),
-
+            // url: "https://github.com/qtrust-id/SeQure-iOS-SDK/releases/download/2.3.0-beta.1/SequreSDK.xcframework.zip",
+            // checksum: "39223f7ecb5bcadae39f4db532fd38782052422a7cfb1b6133a7a032a441a29e"
+            path: "binary/SequreSDK.xcframework"
+        )
     ],
     swiftLanguageVersions: [
       .v5
